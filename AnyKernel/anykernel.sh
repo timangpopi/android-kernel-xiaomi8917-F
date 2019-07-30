cc840d05349860b7585d9dc964270ba041d3c353
timangpopi <53063799+timangpopi@users.noreply.github.com>
2019-07-30 08:46:34 +0700

    Teach me plox

--- AnyKernel/anykernel.sh
+++ AnyKernel/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=BlueOceand //@Mmhmdfas//
+kernel.string=BlueOceans //@Mmhmdfas//
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
